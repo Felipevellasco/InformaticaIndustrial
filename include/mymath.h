966d3c536d33198268df7a7a85e2697c24c3856c
Felipe Vellasco <vellascofelipe@gmail.com>
2026-08-26 16:55:13 -0300

    aula 2: exercicio final do slide

---
+++ include/mymath.h
@@ -0,0 +1,11 @@
+#ifndef MYMATH_H
+#define MYMATH_H
+int fact(int input);
+
+float y(float x);
+
+float potencia(float, int );
+int soma(int, int);
+int soma(int, int, int);
+float soma (int, float);
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,77 +1,15 @@
-#include <cmath>
-#include <cstdint>
 #include <iostream>
 
+#include "mymath.h"
+
 int main() {
-  std::cout << "Hello World!" << std::endl;
 
-  float peso = 0;
-  float altura = 0;
+float inputs[] = {25, static_cast<float>(fact(7)), potencia(2.5, 3)};
 
-  while (true) {
-    std::cout << "Digite o seu peso em kg:\n";
-    std::cin >> peso;
-    if (peso < 0) {
-      std::cout << "Peso inválido inserido.\n";
-      continue;
-    }
-    break;
-  }
+  std::cout << fact(3) << std::endl;
 
-  while (true) {
-    std::cout << "Digite a sua altura em metros:\n";
-    std::cin >> altura;
-    if (altura < 0) {
-      std::cout << "Altura inválida inserida.\n";
-      continue;
-    }
-    break;
-  }
-
-  float imc = (peso) / (altura * altura);
-
-  std::cout << "IMC = " << imc << std::endl;
-
-  // Abaixo: < 18.5
-  // Normal < 24.9
-  // Acima: < 29.9
-  // Sobrepeso > 30
-
-  enum imcClass_t : uint8_t {
-    abaixo,
-    normal,
-    acima,
-    sobrepeso,
-  };
-
-  imcClass_t weightClass;
-
-  if (imc >= 30)
-    weightClass = sobrepeso;
-  else if (imc >= 25)
-    weightClass = acima;
-  else if (imc >= 18.5)
-    weightClass = normal;
-  else
-    weightClass = abaixo;
-
-  switch (weightClass) {
-    using enum imcClass_t;
-  case abaixo:
-    std::cout << "Você está abaixo do peso desejado." << std::endl;
-    break;
-  case normal:
-    std::cout << "Você está na faixa de peso normal." << std::endl;
-    break;
-  case acima:
-    std::cout << "Você está acima do peso desejado." << std::endl;
-    break;
-  case sobrepeso:
-    std::cout << "Você está obeso." << std::endl;
-    break;
-  default:
-    std::cerr << "Erro de cálculo!" << std::endl;
-    return 1;
+  for (auto &input : inputs) {
+    std::cout << "y(" << input << ") = " << y(input) << std::endl;
   }
 
   return 0;
